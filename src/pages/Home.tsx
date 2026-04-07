c9f38a9bf4f70e3a07149109b487075c1230be5a
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-04-08 01:13:57 +0200

    basic stopwatch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { useSignal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
+
 export default function Home() {
+    const totalSeconds = useSignal(0);
+    const date = new Date(totalSeconds.value * 1000);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            totalSeconds.value += 1;
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
     return (
         <div>
-            <h1 className="font-bold text-4xl">Home</h1>
+            <h1 className="font-bold text-4xl">Stopwatch</h1>
+            <p>
+                {padded(date.getUTCHours())}:{padded(date.getUTCMinutes())}:
+                {padded(date.getSeconds())}
+            </p>
+            <input type="button" value="Start" />
+            <input type="button" value="A" />
+            <input type="button" value="B" />
         </div>
     );
 }
+
+const padded = (n: number) => n.toString().padStart(2, "0");
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact(), tailwindcss()],
+    plugins: [preact(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
